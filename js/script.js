c38ccb22e94ad2f18ce555f6c329bb510230956f
AnthonyBuncio <anthonyb.ft@gmail.com>
2017-03-10 20:29:36 -0600

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') {
+    return
+  };
+  e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+  var audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+  var key = document.querySelector('div[data-key="' + e.keyCode + '"]');
+  
+  if (!audio) {
+    return
+  };
+  
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play()
+}
+  
+var keys = Array.from(document.querySelectorAll('.key'));
+console.log(keys)
+for (var i = 0; i < keys.length; i++) {
+  keys[i].addEventListener('transitionend', removeTransition)
+}
+
+window.addEventListener('keydown', playSound);
